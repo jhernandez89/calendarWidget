d3d06d13ab865faae2500a01d403bf1030e8374a
Jeff Hernandez <jeff.hernandez@druksys.com>
2019-10-15 13:49:48 -0500

    added months

--- src/App.js
+++ src/App.js
@@ -2,10 +2,45 @@ import React from "react";
 import "./App.css";
 import Accruals from "./Body/Accruals/Accruals.js";
 
+const type = {
+  day: 0,
+  week: 1,
+  month: 2,
+  year: 3
+};
+
+const frequency = 7;
+
+const dayOfWeek = {
+  sunday: 0,
+  monday: 1,
+  tuesday: 2,
+  wednesday: 3,
+  thursday: 4,
+  friday: 5,
+  saturday: 6
+};
+
+const dayOfTheMonth = 15;
+
+const hoursToAdd = 8;
+
+const currentNumberOfHours = 14.6;
+
+const startingDay = new Date("April 15 2016");
+
 function App() {
   return (
     <div className="App">
-      <Accruals />
+      <Accruals
+        type={type.month}
+        frequency={frequency}
+        dayOfWeek={dayOfWeek.friday}
+        startingDay={startingDay}
+        currentNumberOfHours={currentNumberOfHours}
+        hoursToAdd={hoursToAdd}
+        dayOfTheMonth={dayOfTheMonth}
+      />
     </div>
   );
 }
--- src/Body/Accruals/Accruals.js
+++ src/Body/Accruals/Accruals.js
@@ -1,14 +1,76 @@
-import React from "react";
+import React, { useState } from "react";
 import { Calendar } from "react-calendar";
 
-const Accruals = () => {
+const weeksBetween = (d1, d2) => {
+  return Math.round((d2 - d1) / (7 * 24 * 60 * 60 * 1000));
+};
+
+const monthsBetween = (d1, d2) => {
+  var months;
+  months = (d2.getFullYear() - d1.getFullYear()) * 12;
+  months -= d1.getMonth();
+  months += d2.getMonth();
+  return months <= 0 ? 0 : months;
+};
+
+const Accruals = ({
+  type,
+  frequency,
+  dayOfWeek,
+  startingDay,
+  hoursToAdd,
+  currentNumberOfHours,
+  dayOfTheMonth
+}) => {
+  const [result, setResult] = useState(0);
+
+  const handleMonth = date => {
+    const today = new Date(Date.now());
+    const numberOfMonths = monthsBetween(today, date);
+    if (today.getDate() >= dayOfTheMonth) {
+      if (date.getDate() >= dayOfTheMonth) {
+        return numberOfMonths;
+      } else {
+        return numberOfMonths - 1;
+      }
+    } else {
+      if (date.getDate() >= dayOfTheMonth) {
+        return numberOfMonths + 1;
+      } else {
+        return numberOfMonths;
+      }
+    }
+  };
+
+  const handleWeek = date => {
+    const numberOfWeeksBetween = weeksBetween(new Date(Date.now()), date);
+    const weeksBetweenStartingDayAndToday = weeksBetween(startingDay, date);
+    console.log("date", weeksBetweenStartingDayAndToday / frequency);
+  };
+
   const handleChange = date => {
-    console.log("date!", date);
+    switch (true) {
+      case type === 0:
+        // handleDay();
+        break;
+      case type === 1:
+        handleWeek(date);
+        break;
+      case type === 2:
+        setResult(handleMonth(date) * hoursToAdd);
+        break;
+      case type === 3:
+        // handleYear();
+        break;
+      default:
+        console.log("noooo");
+    }
   };
 
   return (
     <div>
       <Calendar onChange={date => handleChange(date)} />
+      <h3>{result}</h3>
     </div>
   );
 };
